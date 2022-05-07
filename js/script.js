c50796d0a8b1d49a3b07dda21b366b9f823edf18
jys <jys2874@naver.com>
2022-05-08 00:21:15 +0900

    변경내용 메모

---
+++ js/script.js
@@ -0,0 +1,38 @@
+ 
+  $(document).ready(function(){
+    $(".main-banner").slick({
+
+      slide: "div",
+      slidesToShow: 1,
+      speed: 1000,
+      arrows: true,
+      autoplay: false,
+      autoplaySpeed: 1000,
+      vertical: false,
+    });
+
+     $(".banner2").slick({
+
+       slide: "div",
+       slidesToShow: 1,
+       speed: 1000,
+       arrows: true,
+       autoplay: false,
+       autoplaySpeed: 1000,
+       vertical: false,
+     });
+     
+     $(".slide-box-wrap").slick({
+
+      slide: "div",
+      slidesToShow: 3,
+      speed: 1000,
+      arrows: true,
+      autoplay: false,
+      autoplaySpeed: 1000,
+      vertical: false,
+    });
+
+
+
+  });
